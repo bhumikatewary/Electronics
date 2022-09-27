aeda530b6ad1ac23a40f56d7b5e73be4cbece3e6
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-09-27 17:01:15 +0530

    Update halfadder.vhd

--- FPGA/Day1/halfadder.vhd
+++ FPGA/Day1/halfadder.vhd
@@ -1,1 +1,12 @@
-
+library IEEE;
+use IEEE.std_logic_1164.all;
+entity half_adder is
+port(A,B: in std_logic;          
+sum,carryout: out std_logic
+); 
+end half_adder;
+architecture flow of half_adder is
+begin
+sum<= A xor B;
+carryout<=A and B;
+end flow;
